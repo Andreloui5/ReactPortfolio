8bf1691a0932f85bc87ef1dc3d7e61412c5087eb
Craig Gant <craigkgant@gmail.com>
2020-03-07 05:17:07 -0500

    added images and a card styled component

---
+++ src/StyledComponents/Card.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Card = styled(motion.div)`
+  border-radius: 15px;
+  padding: 20px;
+  color: white;
+  box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.4);
+  img {
+    width: 100%;
+  }
+`;
+
+export default Card;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Container, Row, Card } from "react-bootstrap";
+import Chicken from "./assets/images/Chicken.png";
+import GameStart from "./assets/images/GameStart.gif";
 
-function Projects(props) {
+const myProjects = [Chicken, GameStart];
+
+function Projects() {
   return (
     <div>
-      <h1>Projects</h1>
+      <Container>
+        <Row>
+          {myProjects.map(item => (
+            <>
+              <Card md={4}>
+                <Card.Img
+                  key={myProjects.indexOf(item)}
+                  src={item}
+                  alt={`App Preview ${myProjects.indexOf(item) + 1}`}
+                  rounded
+                />
+              </Card>
+            </>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }

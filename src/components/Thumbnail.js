ba7fab4df56d6552d13f028f75c6ea83113b419c
Craig Gant <craigkgant@gmail.com>
2020-03-17 12:02:52 -0400

    added styling to thumbnail.js

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -16,11 +16,16 @@ const thumbnailVariants = {
 };
 
 const frameVariants = {
-  hover: { scale: 0.95, borderRadius: "200%" }
+  hover: {
+    scale: 1.05,
+    borderRadius: "60%",
+    boxShadow:
+      "0 2.8px 2.2px rgba(0, 0, 0, 0.02), 0 6.7px 5.3px rgba(0, 0, 0, 0.028), 0 12.5px 10px rgba(0, 0, 0, 0.035), 0 22.3px 17.9px rgba(0, 0, 0, 0.042), 0 41.8px 33.4px rgba(0, 0, 0, 0.05), 0 100px 80px rgba(0, 0, 0, 0.07)"
+  }
 };
 
 const imgVariants = {
-  hover: { scale: 1.15 }
+  hover: { scale: 1.02 }
 };
 
 const Thumbnail = props => {

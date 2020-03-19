8c2c8a2b402fe3507b1aeb379ca8082a35196c61
Craig Gant <craigkgant@gmail.com>
2020-03-19 17:15:21 -0400

    added github links to individual project pages

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import ProjectArray from "../assets/projectArray";
+import FontAwes from "../FontAwes/FontAwes";
+import { Row, Col, Container } from "react-bootstrap";
+import "./style.css";
 
 const transition = {
   duration: 1,
@@ -34,16 +37,45 @@ const ProjectPage = props => {
   console.log(project);
 
   return (
-    <motion.div className="single" initial="exit" animate="enter" exit="exit">
-      <motion.img
-        variants={imageVariants}
-        src={project[0].image}
-        alt={project[0].name}
-      />
-      <motion.div className="back" variants={backVariants}>
-        <Link to="/projects">← Back</Link>
-      </motion.div>
-    </motion.div>
+    <Container fluid>
+      <Row>
+        <Col sm={6}>
+          <motion.div
+            className="single"
+            initial="exit"
+            animate="enter"
+            exit="exit"
+          >
+            <motion.img
+              variants={imageVariants}
+              src={project[0].image}
+              alt={project[0].name}
+            />
+          </motion.div>
+        </Col>
+        <Col sm={6}>
+          <Row>
+            <p>{project[0].description}</p>
+          </Row>
+          <Row>
+            <FontAwes
+              href={project[0].gitHub}
+              target="_blank"
+              className="fab fa-github gitIcon"
+              title="Github"
+            />
+            <motion.div className="back" variants={backVariants}>
+              <Link to="/projects">
+                <FontAwes
+                  className="far fa-arrow-alt-circle-left"
+                  title="Back"
+                />
+              </Link>
+            </motion.div>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/assets/projectArray.js
+++ src/components/assets/projectArray.js
@@ -10,35 +10,40 @@ const projectArray = [
     name: "Attend",
     image: Attend,
     description:
-      "This project takes advantage of a texting service to track attendance at events. A user texts a chosen keyword to the organization's telephone number, and is linked with the event that uses the same keyword. Their data is then dynamically captured and stored in a database, and becomes searchable in the app. The organizers can then review individuals' data and text them within the app to inform about future events, etc. Currently this app uses Clearstream for the texting aspects, but could easily be converted to a company like Twilio, Trumpia, or similar companies. This app was built using the MERN stack."
+      "This project takes advantage of a texting service to track attendance at events. A user texts a chosen keyword to the organization's telephone number, and is linked with the event that uses the same keyword. Their data is then dynamically captured and stored in a database, and becomes searchable in the app. The organizers can then review individuals' data and text them within the app to inform about future events, etc. Currently this app uses Clearstream for the texting aspects, but could easily be converted to a company like Twilio, Trumpia, or similar companies. This app was built using the MERN stack.",
+    gitHub: "https://github.com/Andreloui5/Attendance"
   },
   {
     id: 2,
     name: "Appetite",
     image: Chicken,
     description:
-      "A food app designed to prompt a user to input food or drink ingredients and to supply recipes that include those ingredients."
+      "A food app designed to prompt a user to input food or drink ingredients and to supply recipes that include those ingredients.",
+    gitHub: "https://github.com/cmoraes2013/APPetite-Project"
   },
   {
     id: 3,
     name: "Trivia Game",
     image: GameStart,
     description:
-      "This application is designed to walk the user through a series of trivia questions, and to present a score at the end. Each question is allotted 15seconds— with correct answers adding time to the clock, and incorrect answers subtracting time. The user's time remaining at the end of the questions is their final score."
+      "This application is designed to walk the user through a series of trivia questions, and to present a score at the end. Each question is allotted 15seconds— with correct answers adding time to the clock, and incorrect answers subtracting time. The user's time remaining at the end of the questions is their final score.",
+    gitHub: "https://github.com/Andreloui5/Quiz-Game"
   },
   {
     id: 4,
     name: "Template Engine",
     image: Template,
     description:
-      "This application is designed to dynamically generate an html page that contains contact information for a whole team. The user is prompted to enter the information upon initialization. Once the user adds all members of their team, the application dynamically creates a syled html document containing the information they entered."
+      "This application is designed to dynamically generate an html page that contains contact information for a whole team. The user is prompted to enter the information upon initialization. Once the user adds all members of their team, the application dynamically creates a syled html document containing the information they entered.",
+    gitHub: "https://github.com/Andreloui5/TemplateEngine"
   },
   {
     id: 5,
     name: "Workout App",
     image: Workout,
     description:
-      "This application is designed to allow a user to continually track their fitness goals and to view their workout data in various forms. At the homepage, a user is prompted to either continue an ongoing workout or to begin a new one. They are then asked about the details of the workout they'd like to enter. Some of the data entered is immediately visible on the homepage (like the cumulative duration of a workout), while more data is presented by the charts on the user dashboard."
+      "This application is designed to allow a user to continually track their fitness goals and to view their workout data in various forms. At the homepage, a user is prompted to either continue an ongoing workout or to begin a new one. They are then asked about the details of the workout they'd like to enter. Some of the data entered is immediately visible on the homepage (like the cumulative duration of a workout), while more data is presented by the charts on the user dashboard.",
+    gitHub: "https://github.com/Andreloui5/NoSQL_WorkoutApp"
   }
 ];
 
